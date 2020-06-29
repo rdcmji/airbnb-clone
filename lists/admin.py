bfaf2a0d8cc8803e641a2a5324716b7bb4f7f809
rdcmji <rdcmji.gmail.com>
2020-06-29 22:46:51 +0900

    Add List Model

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.List)
+class listAdmin(admin.ModelAdmin):
+
+    """ List Admin Definition """
+
+    pass
